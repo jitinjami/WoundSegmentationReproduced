cedd28afb474dbd5b124f44299011074a7412d89
jitinjami <jitinjami1@gmail.com>
2024-03-07 16:26:22 +0100

    train scripts

--- train_linknet.sh
+++ train_linknet.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=work
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=mobv2
+#SBATCH --job-name=linknet
 
 module load cuda/11.8.0
 module load python/3.8-anaconda
 conda activate seg
-cd /home/hpc/iwso/iwso114h/WoundSegmentationWithMobilenetV2/
-python3 main.py
+cd /home/hpc/iwso/iwso114h/~/WoundSegmentationReproduced/
+python3 main.py --no-mnv2
---
+++ train_mobv2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --clusters=tinygpu
+#SBATCH --partition=work
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=mobv2
+
+module load cuda/11.8.0
+module load python/3.8-anaconda
+conda activate seg
+cd /home/hpc/iwso/iwso114h/~/WoundSegmentationReproduced/
+python3 main.py --mnv2
